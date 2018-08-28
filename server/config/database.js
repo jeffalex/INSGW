fd305a92ecdb8b48de1d1568d91dcf0a2a7ca3d0
jeffalex <jeffreysantanapolanco@gmail.com>
2018-08-28 12:33:13 -0400

    Adding test config & Fixing issues

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 module.exports = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
-  process.env.DB_PASS, {
+  process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     define: {
